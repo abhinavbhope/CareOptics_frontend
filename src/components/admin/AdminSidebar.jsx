086029aca0987c042b2c27303dafab109f6b2d5c
abhinavbhope <abhinavdecide@gmail.com>
2025-09-12 13:41:42 +0530

    admin side bar changes

--- src/components/admin/AdminSidebar.jsx
+++ src/components/admin/AdminSidebar.jsx
@@ -50,8 +50,7 @@ export function AdminSidebar() {
     }
 
     return (
-        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-             <TooltipProvider>
+<aside className="fixed inset-y-0 left-0 z-10 flex w-14 flex-col border-r bg-background">             <TooltipProvider>
             <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
                 <Link
                     href="/"
